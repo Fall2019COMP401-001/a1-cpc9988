09a717ebfb3f5014bcadd29bd1aef8afd6690fab
cpc99 <cpc99@DESKTOP-PH322M1.wireless-1x.unc.edu>
2019-08-28 21:13:20 -0400

    first draft

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A1Adept {
@@ -8,7 +9,141 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// Read in count of store products to process
+		
+		int productcount = scan.nextInt();
+		
+		//Create separate arrays to store product names,
+		//and their respective prices.  
+				
+		String[] productnames = new String[productcount];
+		double[] productprices = new double[productcount];
+		
+		//Read values into arrays
+		
+		for (int i=0; i < productcount; i++) {
+			productnames[i] = scan.next();
+			productprices[i] = scan.nextDouble();
+		}
+		
+		//Read in count of customers to process
+		int customercount = scan.nextInt();
+		
+		//Create separate arrays to store customers' first name,
+		//last name, number of items bought.  
+				
+		String[] FirstNames = new String[customercount];
+		String[] LastNames = new String[customercount];
+		int[] NumofItems = new int[customercount];
+		double[] totalcosts = new double[customercount];
+		
+		//Read values into arrays 
+		
+		for (int i=0; i < customercount; i++) {
+			FirstNames[i] = scan.next();
+			LastNames[i] = scan.next();
+			NumofItems[i] = scan.nextInt();
+			
+			int[] itemquantity = new int[NumofItems[i]];
+			String[] itemname = new String[NumofItems[i]];
+			double[] itemprice = new double[NumofItems[i]];
+		
+			
+			for (int j=0; j < NumofItems[i]; j++) {
+				itemquantity[j] = scan.nextInt();
+				itemname[j] = scan.next();
+				int productidx = Arrays.asList(productnames).indexOf(itemname[j]);
+				itemprice[j] = productprices[productidx];
+			}
+			
+			double[] totalcosts = new double[customercount];
+			double finalcost = A1Novice.calculateTotalCost(itemquantity, itemprice);
+			for (int k=0; k < customercount; k++) {
+				totalcosts[k] = finalcost
+			}
+			
+		}
+		// All input parsed, so close scanner
+		scan.close();
+		
+		double maxcost = findValueMax(totalcosts);
+		double mincost = findValueMin(totalcosts);
+		double sum = calculateValueSum(totalcosts);
+		double averagecost = ((double) sum) / ((double) customercount);
+		int maxidx = Arrays.asList(totalcosts).indexOf(maxcost);
+		int minidx = Arrays.asList(totalcosts).indexOf(maxcost);
+		
+		System.out.println("Biggest: " + FirstNames[maxidx] + " " + LastNames[maxidx] + " (" + String.format("%.2f", maxcost) + ")");
+		System.out.println("Smallest: " + FirstNames[minidx] + " " + LastNames[minidx] + " (" + String.format("%.2f", mincost) + ")");
+		System.out.println("Average: " + String.format("%.2f", averagecost));
 		
 	}
+	
+	static double findValueMin(double[] vals) {
+		
+		// Initialize current minimum to first value in array.
+		double cur_min = vals[0];
+		
+		// Starting with second value (if any), compare each value
+		// in array with current minimum and replace if smaller.
+		
+		for (int i=1; i < vals.length; i++) {
+			if (vals[i] < cur_min) {
+				cur_min = vals[i];
+			}
+		}
+		
+		return cur_min;
+	}
+
+	/* findValueMax
+	 * Finds and returns the maximum value in an array of integers
+	 * 
+	 * Input: array of integers vals
+	 * 
+	 * Output: maximum value found within the array
+	 * 
+	 * Preconditions:
+	 * Input array must not be null and must contain at least one value.
+	 */
+	
+	static double findValueMax(double[] vals) {
+		
+		// Initialize current minimum to first value in array.
+		double cur_max = vals[0];
+		
+		// Starting with second value (if any), compare each value
+		// in array with current minimum and replace if smaller.
+		
+		for (int i=1; i < vals.length; i++) {
+			if (vals[i] > cur_max) {
+				cur_max = vals[i];
+			}
+		}
+		
+		return cur_max;
+	}
+
+	/* calculateValueSum 
+	 * Calculates the sum on an array of integers
+	 *
+	 * Input: array of integers vals
+	 * 
+	 * Output: integer sum of values in vals
+	 * 
+	 * Preconditions:
+	 * Input array must not be null.
+	 */
+	
+	static double calculateValueSum(double[] vals) {
+		
+		double sum = 0;
+		
+		for (int i=0; i<vals.length; i++) {
+			sum += vals[i];
+		}
+		
+		return sum;
+	}
+		
 }
