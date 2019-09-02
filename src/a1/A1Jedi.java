ccd38c726cc346eef1b37aabf0a3789616a9fb1c
cpc99 <cpc99@DESKTOP-PH322M1.wireless-1x.unc.edu>
2019-09-02 03:53:32 -0400

    first draft of jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A1Jedi {
@@ -8,7 +9,79 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// Read in count of store products to process
+		
+		int productcount = scan.nextInt();
+				
+		//Create separate arrays to store product names,
+		//and their respective prices.  
+						
+		String[] productnames = new String[productcount];
+		double[] productprices = new double[productcount];
+		
+		//Read values into arrays
+		
+		for (int i=0; i < productcount; i++) {
+			productnames[i] = scan.next();
+			productprices[i] = scan.nextDouble();
+		}
+				
+		//Read in count of customers to process
+		int customercount = scan.nextInt();
+		
+		//Create separate arrays to store customers' first name,
+		//last name, number of items bought.  
+						
+		String[] FirstNames = new String[customercount];
+		String[] LastNames = new String[customercount];
+		int[] NumofItems = new int[customercount];
+		int[] totalbought = new int[productcount];
+		int[] customerpurchasecount = new int[productcount];
+		
+		//Read values into arrays 
+		
+		for (int i=0; i < customercount; i++) {
+			FirstNames[i] = scan.next();
+			LastNames[i] = scan.next();
+			NumofItems[i] = scan.nextInt();
+					
+			int[] itemquantity = new int[NumofItems[i]];
+			String[] itemname = new String[NumofItems[i]];
+				
+					
+			for (int j=0; j < NumofItems[i]; j++) {
+				itemquantity[j] = scan.nextInt();
+				itemname[j] = scan.next();
+				int productidx = Arrays.asList(productnames).indexOf(itemname[j]);
+				totalbought[productidx] += itemquantity[j];
+
+			}
+			
+			
+			
+			for (int k=0; k < productcount; k++) {
+				if (Arrays.asList(itemname).contains(productnames[k])) {
+					//int itemidx = Arrays.asList(itemname).indexOf(productnames[k]);
+					//int itemqty = itemquantity[itemidx];
+					//totalbought[k] += itemqty;
+					customerpurchasecount[k] += 1;
+				} 
+			
+				
+			}
+			
+		}
+		// All input parsed, so close scanner
+		scan.close();
+		
+		for (int f=0; f < productcount; f++) {
+			if (customerpurchasecount[f] == 0) {
+				System.out.println("No customers bought " + productnames[f]);
+			} else {
+				System.out.println(customerpurchasecount[f] + " customers bought " + totalbought[f] + " " + productnames[f]);
+			}
+		}
 		
 	}
+
 }
